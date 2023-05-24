919ce26ba75df799144e98b77139885dc26f90b5
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-24 16:54:34 +0530

    Fine Tune Dice Game

--- 04_Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++ 04_Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -1,24 +1,26 @@
 // Generating Random Numbers
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let n1 = Math.floor(Math.random() * 6) + 1;
+let n2 = Math.floor(Math.random() * 6) + 1;
 
 // Changing Images
+let image1 = document.querySelector(".img1");
+// OR
+// let image1 = document.getElementsByClassName("img1")[0];
 
-var image1 = document.querySelector(".img1");
-var image2 = document.querySelector(".img2");
+let image2 = document.querySelector(".img2");
 
-image1.src = "images/dice" + randomNumber1 + ".png";
-image2.src = "images/dice" + randomNumber2 + ".png";
+image1.src = "images/dice" + n1 + ".png";
+// OR (template string)
+// image1.src = `images/dice${n1}.png`;
+
+image2.src = "images/dice" + n2 + ".png";
 
 // Checking which Player Won
-
-var heading = document.querySelector("h1");
-
-if (randomNumber1 > randomNumber2) {
-  heading.innerHTML = "🚩Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-  heading.innerHTML = "Player 2 Wins!🚩";
+let heading = document.querySelector("h1");
+if (n1 > n2) {
+  heading.innerHTML = "🚩 Player 1 Wins!";
+} else if (n1 < n2) {
+  heading.innerHTML = "Player 2 Wins! 🚩";
 } else {
   heading.innerHTML = "Draw!";
 }
