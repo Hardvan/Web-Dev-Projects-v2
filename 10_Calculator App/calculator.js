6f10043baff92102153fc79c2af969f95519ba89
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-24 17:34:14 +0530

    Fine Tune Calculator App

--- 10_Calculator App/calculator.js
+++ 10_Calculator App/calculator.js
@@ -1,9 +1,13 @@
+// ? To start the server:
+// * node calculator.js
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// GET requests
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -11,19 +15,20 @@ app.get("/bmicalculator", function (req, res) {
   res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
+// POST requests
 app.post("/", function (req, res) {
-  var num1 = Number(req.body.num1);
-  var num2 = Number(req.body.num2);
-  var result = num1 + num2;
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
 
-  res.send("The addition of " + num1 + " + " + num2 + " = " + result);
+  res.send(`The addition of ${num1} + ${num2} = ${result}`);
 });
 app.post("/bmicalculator", function (req, res) {
-  var weight = parseFloat(req.body.weight);
-  var height = parseFloat(req.body.height);
-  var result = weight / (height * height);
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let result = weight / (height * height);
 
-  res.send("Your BMI is " + result);
+  res.send(`Your BMI is ${result}`);
 });
 
 app.listen(3000, function () {
