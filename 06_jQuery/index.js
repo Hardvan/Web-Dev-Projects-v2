c18d88d211a75deb9dbc0598f1ac6ad1e8788679
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-24 17:17:03 +0530

    Fine tune jQuery

--- 06_jQuery/index.js
+++ 06_jQuery/index.js
@@ -2,6 +2,10 @@ $("h1").on("mouseover", function () {
   $("h1").css("color", "purple");
 });
 
+$("h1").on("mouseleave", function () {
+  $("h1").css("color", "yellow");
+});
+
 $("button").on("click", function () {
   $("h1").toggle();
 });
